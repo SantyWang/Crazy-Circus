fddde6c799ea637d1051ef5c400c253c11e1bfa8
Santy Wang <wh19930829@qq.com>
2024-07-06 12:49:32 +0800

    Player Use Dynamic body

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour
 
     private List<GameObject> allCarriedCrates = new List<GameObject>();
     private Animator animator;
+    Rigidbody rigidBody;
 
     // Start is called before the first frame update
     void Start()
@@ -23,94 +24,20 @@ public class PlayerController : MonoBehaviour
         animator = GetComponent<Animator>();
         animator.SetFloat("run", 1.0f);
         animator.SetBool("gr", true);
+        rigidBody = GetComponent<Rigidbody>();
     }
-    bool isColliding = false;
-    // Update is called once per frame
+
     void Update()
     {
         if (Input.GetKey(moveLeftKey))
         {
-
-            // 如果发生了碰撞，且是player1，那么不允许朝着player2的方向移动
-            if (isColliding)
-            {
-                GameObject player1 = GetPlayer1();
-                GameObject player2 = GetPlayer2();
-                if (player1 == null || player2 == null)
-                {
-                    return;
-                }
-                Vector3 player1Pos = player1.transform.position;
-                Vector3 player2Pos = player2.transform.position;
-
-                Vector3 nextDirection = new Vector3(0, 0, horizontalSpeed * Time.deltaTime);
-                if (IsPlayer1(gameObject))
-                {
-                    // 如果移动方向和player2的方向相同，那么不允许移动
-                    Vector3 direction = player2Pos - player1Pos;
-                    if (Vector3.Dot(nextDirection, direction) > 0)
-                    {
-                        return;
-                    }
-                }
-
-                if (IsPlayer2(gameObject))
-                {
-                    // 如果移动方向和player1的方向相同，那么不允许移动
-                    Vector3 direction = player1Pos - player2Pos;
-                    if (Vector3.Dot(nextDirection, direction) > 0)
-                    {
-                        return;
-                    }
-                }
-
-            }
-            transform.localPosition += new Vector3(0, 0, horizontalSpeed * Time.deltaTime);
-            animator.SetFloat("turn", -1);
+            rigidBody.AddForce(transform.right * -horizontalSpeed, ForceMode.Force);
         }
         else if (Input.GetKey(moveRightKey))
         {
-
-            if (isColliding)
-            {
-                GameObject player1 = GetPlayer1();
-                GameObject player2 = GetPlayer2();
-                if (player1 == null || player2 == null)
-                {
-                    return;
-                }
-                Vector3 nextDirection = new Vector3(0, 0, -horizontalSpeed * Time.deltaTime);
-                
-                Vector3 player1Pos = player1.transform.position;
-                Vector3 player2Pos = player2.transform.position;
-                if (IsPlayer1(gameObject))
-                {
-                    // 如果移动方向和player2的方向相同，那么不允许移动
-                    Vector3 direction = player2Pos - player1Pos;
-                    if (Vector3.Dot(nextDirection, direction) > 0)
-                    {
-                        return;
-                    }
-                }
-
-                if (IsPlayer2(gameObject))
-                {
-                    // 如果移动方向和player1的方向相同，那么不允许移动
-                    Vector3 direction = player1Pos - player2Pos;
-                    if (Vector3.Dot(nextDirection, direction) > 0)
-                    {
-                        return;
-                    }
-                }
-
-            }
-            transform.localPosition += new Vector3(0, 0, -horizontalSpeed * Time.deltaTime);
-            animator.SetFloat("turn", 1);
+            rigidBody.AddForce(transform.right * horizontalSpeed, ForceMode.Force);
         }
-        else
-        {
-            animator.SetFloat("turn", 0);
-        }
+        animator.SetFloat("turn", -rigidBody.velocity.z);
     }
 
     private void AddCrate()
@@ -118,14 +45,7 @@ public class PlayerController : MonoBehaviour
         GameObject newCrate = GameObject.Instantiate(carriedCratePrefab, carriedCratePivot);
         newCrate.transform.localPosition = new Vector3(0, allCarriedCrates.Count * 0.398f, 0);
         allCarriedCrates.Add(newCrate);
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            isColliding = false;
-        }
+        rigidBody.mass += newCrate.GetComponent<CarriedCrateController>().mass;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -135,34 +55,5 @@ public class PlayerController : MonoBehaviour
             GameObject.Destroy(other.gameObject);
             AddCrate();
         }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            isColliding = true;
-        }
-    }
-
-
-    private bool IsPlayer1(GameObject go)
-    {
-        return go.name.IndexOf("Player1") > -1;
-    }
-
-    private bool IsPlayer2(GameObject go)
-    {
-        return go.name.IndexOf("Player2") > -1;
-    }
-
-    private GameObject GetPlayer1()
-    {
-        return GameObject.FindGameObjectsWithTag("Player").Where(IsPlayer1).FirstOrDefault();
-    }
-
-    private GameObject GetPlayer2()
-    {
-        return GameObject.FindGameObjectsWithTag("Player").Where(IsPlayer2).FirstOrDefault();
     }
 }
