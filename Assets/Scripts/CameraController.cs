340cd44d89ed0e85aa4ba331254948afc5268e11
Santy Wang <wh19930829@qq.com>
2024-07-07 11:50:28 +0800

    add skybox and adjust physics

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,17 +7,26 @@ public class CameraController : MonoBehaviour
     GameMode gameMode;
     Vector3 initialPos;
     Vector3 targetPos;
+    Skybox skyBox;
+    float skyboxRotation = 0;
     // Start is called before the first frame update
     void Start()
     {
         gameMode = GameObject.Find("GameMode").GetComponent<GameMode>();
         initialPos = transform.position;
         targetPos = transform.position;
+        skyBox = GetComponent<Skybox>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        skyboxRotation += Time.deltaTime;
+        if (skyboxRotation > 360.0)
+        {
+            skyboxRotation -= 360f;
+        }
+        skyBox.material.SetFloat("_Rotation", skyboxRotation);
         if (!gameMode.isGameRunning)
         {
             return;
@@ -25,5 +34,6 @@ public class CameraController : MonoBehaviour
         float crateNums = Mathf.Max(gameMode.player.allCarriedCrates.Count, gameMode.player2.allCarriedCrates.Count) - gameMode.player.initialCrateNum;
         targetPos = initialPos - transform.forward * crateNums * 0.75f;
         transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime);
+        
     }
 }
--- Assets/Scripts/CarriedCrateController.cs
+++ Assets/Scripts/CarriedCrateController.cs
@@ -2,27 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CarriedCrateController : MonoBehaviour
 {
 
     public float mass = 0.1f;
+    [HideInInspector]
+    public PlayerController playerController;
+    [HideInInspector]
+    public float movingSpeed;
+    Rigidbody rigidBody;
+    bool isThrow = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (!isThrow)
+        {
+            return;
+        }
+        rigidBody.MovePosition(transform.position + Vector3.left * movingSpeed * Time.deltaTime);
+
+        if (transform.position.y < -300)
+        {
+            Destroy(this.gameObject);
+        }
     }
+
     void OnJointBreak(float breakForce)
     {
         // 需要知道所在的player
         Debug.Log("Crate is broken");
-        var root = this.GetComponent<ConfigurableJoint>().connectedBody.transform.parent.root.GetComponent<PlayerController>();
-        root.OnPlayerJointBreak(breakForce);
+        if (playerController == null)
+        {
+            return;
+        }
+        var index = playerController.allCarriedCrates.IndexOf(gameObject);
+        if (index >= 0)
+        {
+            for (int i = playerController.allCarriedCrates.Count; i > index; i--)
+            {
+                playerController.RemoveCrate();
+            }
+        }
+
+    }
+
+    public void ThrowAway ()
+    {
+        isThrow = true;
     }
 }
