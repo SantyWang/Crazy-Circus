d71276184f5968535068511e5621e780c6579401
moshuying <1460083332@qq.com>
2024-07-07 15:02:28 +0800

    添加一定的偏移确保初始时地面绘制正常

--- Assets/Scripts/MovingRoadController.cs
+++ Assets/Scripts/MovingRoadController.cs
@@ -10,6 +10,14 @@ public class MovingRoadController : MonoBehaviour
     [HideInInspector]
     public float movingSpeed;
 
+    [HideInInspector]
+    public Vector3 offest = new Vector3(0,0,0);
+
+    private void Start()
+    {
+        offest = spawner.moveDirection * movingSpeed;
+        transform.Translate(offest * 20);
+    }
     void Update()
     {
         transform.Translate(spawner.moveDirection * movingSpeed * Time.deltaTime);
